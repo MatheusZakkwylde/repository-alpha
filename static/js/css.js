0f0821b2b08d6a5be00784e4474e6b4accf83948
MatheusZakkwylde <matheuszakkwylde360@gmail.com>
2023-08-14 22:04:07 -0300

    Adiconando links javascript e css em arquivos css

---
+++ static/js/css.js
@@ -0,0 +1,13 @@
+//ICONE PARA CELULAR
+document.write('<link rel="apple-touch-icon" sizes="76x76" href="../static/img/lobo-icon.webp">');
+//icone para site
+document.write('<link rel="icon" type="image/png" href="../static/img/lobo-icon.webp">');
+document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">');
+document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">');
+document.write('<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">');
+document.write('<link rel="stylesheet" href="../static/css/template.css">');
+document.write('<link rel="stylesheet" href="../static/css/navbar.css">');
+document.write('<link rel="stylesheet" href="../static/css/sidebar.css">');
+document.write('<link rel="stylesheet" href="../static/css/accordion.css">');
+document.write('<link rel="stylesheet" href="../static/css/modal.css">');
+document.write('<link rel="stylesheet" href="../static/css/icon.css">');
---
+++ static/js/src.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+// jQuery
+document.write('<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"><\/script>');
+
+// Seus outros scripts
+document.write('<script src="../static/js/url.js"><\/script>');
+document.write('<script src="../static/js/home.js"><\/script>');
+document.write('<script src="../static/js/sidebar.js"><\/script>');
+document.write('<script src="../static/js/modal.js"><\/script>');
+document.write('<script src="../static/js/validation.js"><\/script>');
+
+// Popper.js
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"><\/script>');
+
+// Bootstrap
+document.write('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"><\/script>');
+document.write('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"><\/script>');
