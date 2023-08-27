f63f3c0f6ff130c0b257b70b7b3158f05f60eb9b
MatheusZakkwylde <matheuszakkwylde360@gmail.com>
2023-08-27 19:22:14 -0300

    Adicionando os setores

--- static/js/home.js
+++ static/js/home.js
@@ -7,11 +7,17 @@
         $('.body-abstract-one').removeClass('col-lg-3').addClass('col-lg-1');
         $('.body-abstract').removeClass('col-lg-3').addClass('col-lg-2');
         $('.body-dimension').removeClass('col-lg-3').addClass('col-lg-4');
+        $('.body-abstract-fila-one').removeClass('col-lg-2').addClass('col-lg-0');
+        $('.body-dimension-fila').removeClass('col-lg-5').addClass('col-lg-6');
+        $('.body-dimension-fila-one').removeClass('col-lg-4').addClass('col-lg-5');
         expanded = false;
       } else {
         $('.body-abstract-one').removeClass('col-lg-1').addClass('col-lg-3');
         $('.body-abstract').removeClass('col-lg-2').addClass('col-lg-3');
         $('.body-dimension').removeClass('col-lg-4').addClass('col-lg-3');
+        $('.body-abstract-fila-one').removeClass('col-lg-0').addClass('col-lg-2');
+        $('.body-dimension-fila').removeClass('col-lg-6').addClass('col-lg-5');
+        $('.body-dimension-fila-one').removeClass('col-lg-5').addClass('col-lg-4');
         expanded = true;
       }
     });
