d3e4f3b5684bcf4833c1c54c946e4d24c9eddcb5
MatheusZakkwylde <matheuszakkwylde360@gmail.com>
2023-10-23 19:56:22 -0300

    Adicionando filtro de mac

--- static/js/validation.js
+++ static/js/validation.js
@@ -25,3 +25,28 @@
     }
   });
 });
+
+
+function transformMac() {
+  const macInput = document.querySelector("#macAddress");
+  const macAddress = macInput.value;
+
+  if (macAddress) {
+    macInput.value = macAddress.replace(/:/g, '').toUpperCase();
+  } else {
+    macInput.value = "MAC inválido.";
+  }
+}
+
+// Adiciona um ouvinte de evento de input para detectar mudanças no campo de entrada
+const macInput = document.querySelector("#macAddress");
+macInput.addEventListener("input", transformMac);
+
+
+// Adicione um ouvinte de evento ao botão
+const clearInputButton = document.querySelector("#btn-clear-input");
+clearInputButton.addEventListener("click", function () {
+  const macInput = document.querySelector("#macAddress");
+  macInput.value = ""; // Limpa o valor do campo de entrada
+});
+
